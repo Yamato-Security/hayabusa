5b47ba397dc1f4b44ecd478287d6fff142a11b30
itiB <is0312vx@ed.ritsumei.ac.jp>
2020-10-22 18:47:59 +0900

    Solve threads

--- src/detections/print.rs
+++ src/detections/print.rs
@@ -2,77 +2,73 @@ use std::collections::HashMap;
 
 #[derive(Debug)]
 pub enum Lang {
-    ja,
-    en,
+    Ja,
+    En,
 }
 
 #[derive(Debug)]
-pub struct MessageLanguages {
+pub struct MessageText {
     ja: String,
     en: String,
 }
 
 #[derive(Debug)]
-pub struct ErrorMessage {
-    map: HashMap<String, MessageLanguages>,
+pub struct Message {
+    map: HashMap<String, MessageText>,
 }
 
-impl ErrorMessage {
+impl Message {
     pub fn new() -> Self {
-        let mut messages: HashMap<String, MessageLanguages> = HashMap::new();
+        let mut messages: HashMap<String, MessageText> = HashMap::new();
         messages.insert(
             "undefined".to_string(),
-            MessageLanguages {
+            MessageText {
                 ja: "未設定".to_string(),
                 en: "Undefined".to_string(),
             },
         );
-        ErrorMessage { map: messages }
+        Message { map: messages }
     }
 
-    /// エラーメッセージを設定
-    pub fn insert_rule(&mut self, error_code: String, message: MessageLanguages) {
+    /// メッセージを設定
+    pub fn insert_message(&mut self, error_code: String, message: MessageText) {
         self.map.insert(error_code, message);
     }
 
-    /// エラーメッセージを指定された言語で返す
+    /// メッセージを指定された言語で返す
     pub fn return_error_message(&self, error_num: &str, lang: Lang) -> String {
-        let messages = if let Some(boxed_message) = self.map.get(error_num) {
-            boxed_message
-        } else {
-            self.map.get("undefined").unwrap()
-        };
+        let messages = self.map.get(error_num).unwrap_or(self.map.get("undefined").unwrap());
         match lang {
-            Lang::ja => messages.ja.clone(),
-            Lang::en => messages.en.clone(),
+            Lang::Ja => messages.ja.clone(),
+            Lang::En => messages.en.clone(),
         }
     }
 }
 
 #[test]
 fn test_create_error_message() {
-    let mut error_message = ErrorMessage::new();
+    let mut error_message = Message::new();
 
-    error_message.insert_rule(
+    error_message.insert_message(
         "4103".to_string(),
-        MessageLanguages {
+        MessageText {
             ja: "パイプライン実行をしています".to_string(),
             en: "Execute pipeline".to_string(),
         },
     );
 
-    let message_ja1 = error_message.return_error_message("4103", Lang::ja);
+    let message_ja1 = error_message.return_error_message("4103", Lang::Ja);
     assert_eq!(message_ja1, "パイプライン実行をしています");
-    let message_ja2 = error_message.return_error_message("4103", Lang::ja);
+    let message_ja2 = error_message.return_error_message("4103", Lang::Ja);
     assert_eq!(message_ja2, "パイプライン実行をしています");
 
-    let message_en1 = error_message.return_error_message("4103", Lang::en);
+    let message_en1 = error_message.return_error_message("4103", Lang::En);
     assert_eq!(message_en1, "Execute pipeline");
-    let message_en2 = error_message.return_error_message("4103", Lang::en);
+    let message_en2 = error_message.return_error_message("4103", Lang::En);
     assert_eq!(message_en2, "Execute pipeline");
 
-    let undef_ja = error_message.return_error_message("HOGE", Lang::ja);
+    let undef_ja = error_message.return_error_message("HOGE", Lang::Ja);
     assert_eq!(undef_ja, "未設定");
-    let undef_en = error_message.return_error_message("HOGE", Lang::en);
+    let undef_en = error_message.return_error_message("HOGE", Lang::En);
     assert_eq!(undef_en, "Undefined");
 }
