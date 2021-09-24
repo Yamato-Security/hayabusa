c22bc46e22a9295c85dbc94647ff5ccd41e02f13
akiranishikawa <info@i-wind.jp>
2021-09-24 13:11:01 +0900

    add slack notify

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,5 @@ pub mod afterfact;
 pub mod detections;
 pub mod omikuji;
 pub mod timeline;
+pub mod notify;
 pub mod yaml;
---
+++ src/notify/slack.rs
@@ -0,0 +1,55 @@
+extern crate slack_hook;
+use slack_hook::{Slack, PayloadBuilder};
+use dotenv::dotenv;
+use std::env;
+
+pub struct SlackNotify {}
+
+impl SlackNotify {
+
+    // Check if Slack is configured.
+    pub fn check_setting() -> bool {
+        dotenv().ok();
+        if env::var("CHANNEL").is_err() {
+            eprintln!("Channel not found");
+            return false;
+        }
+
+        if env::var("WEBHOOK_URL").is_err() {
+            eprintln!("WEBHOOK_URL not found");
+            return false;
+        }
+        return true;
+    }
+
+    // send message to slack.
+    pub fn notify(msg: String) -> Result<(), String> {
+        dotenv().ok();
+        if !SlackNotify::check_setting() {
+            return Ok(());
+        }
+
+        let channel = env::var("CHANNEL").expect("CHANNEL is not found");
+        let webhook_url = env::var("WEBHOOK_URL").expect("WEBHOOK_URL is not found");
+        let ret = SlackNotify::_send_to_slack(msg, &channel, &webhook_url);
+        if ret.is_ok() {
+            Ok(())
+        }
+        else {
+            Err("Slack Notification Failed.".to_string())
+        }
+    }
+
+    fn _send_to_slack(msg: String, channel: &str, webhook_url: &str) -> Result<(), slack_hook::Error> {        
+        let slack = Slack::new(webhook_url).unwrap();
+        let p = PayloadBuilder::new()
+            .text(msg)
+            .channel(channel)
+            .username("Subdomain Takerover Bot")
+            .icon_emoji(":scream:")
+            .build()
+            .unwrap();
+
+        slack.send(&p)
+    }
+}
