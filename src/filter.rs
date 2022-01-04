8becdacf91907aab6d32668c571d78661fd8226b
kazuminn <warugaki.k.k@gmail.com>
2022-01-04 23:07:57 +0900

    add error handling and split function

--- src/filter.rs
+++ src/filter.rs
@@ -1,6 +1,10 @@
 use crate::detections::configs;
+use crate::detections::print::AlertMessage;
+use crate::detections::print::ERROR_LOG_STACK;
+use crate::detections::print::QUIET_ERRORS_FLAG;
 use std::collections::HashSet;
 use std::fs::File;
+use std::io::BufWriter;
 use std::io::{BufRead, BufReader};
 
 #[derive(Clone, Debug)]
@@ -13,40 +17,49 @@ pub fn exclude_ids() -> RuleExclude {
         no_use_rule: HashSet::new(),
     };
 
-    let f = match File::open("config/exclude-rules.txt") {
-        Ok(file) => file,
-        Err(_) => panic!("config/exclude-rules.txt does not exist"),
-    };
-    let reader = BufReader::new(f);
-    for v in reader.lines() {
-        let v = v.unwrap().split("#").collect::<Vec<&str>>()[0].trim().to_string();
-        if v.is_empty() {
-            // 空行は無視する。
-            continue;
-        }
-        exclude_ids.no_use_rule.insert(v);
-    }
-
     if !configs::CONFIG
         .read()
         .unwrap()
         .args
         .is_present("enable-noisy-rules")
     {
-        let f = match File::open("config/noisy-rules.txt") {
-            Ok(file) => file,
-            Err(_) => panic!("config/noisy-rules.txt does not exist"),
-        };
-        let reader = BufReader::new(f);
+        exclude_ids.insert_ids("config/noisy-rules.txt");
+    };
+
+    exclude_ids.insert_ids("config/exclude-rules.txt");
+
+    return exclude_ids;
+}
+
+impl RuleExclude {
+    fn insert_ids(&mut self, filename: &str) {
+        let f = File::open(filename);
+        if f.is_err() {
+            if configs::CONFIG.read().unwrap().args.is_present("verbose") {
+                AlertMessage::alert(
+                    &mut BufWriter::new(std::io::stderr().lock()),
+                    &format!("[ERROR] {}", f.as_ref().unwrap_err()),
+                )
+                .ok();
+            }
+            if !*QUIET_ERRORS_FLAG {
+                ERROR_LOG_STACK
+                    .lock()
+                    .unwrap()
+                    .push(format!("[ERROR] {}", f.as_ref().unwrap_err()));
+            }
+            return ();
+        }
+        let reader = BufReader::new(f.unwrap());
         for v in reader.lines() {
-            let v = v.unwrap().split("#").collect::<Vec<&str>>()[0].trim().to_string();
+            let v = v.unwrap().split("#").collect::<Vec<&str>>()[0]
+                .trim()
+                .to_string();
             if v.is_empty() {
                 // 空行は無視する。
                 continue;
             }
-            exclude_ids.no_use_rule.insert(v);
+            self.no_use_rule.insert(v);
         }
-    };
-
-    return exclude_ids;
+    }
 }
