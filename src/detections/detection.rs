962136cb372de9df329f2127d558f7838ae85616
Alan Smithee <nextsasasa@gmail.com>
2022-02-10 14:33:24 +0900

    changed tag info separator #234

--- src/detections/detection.rs
+++ src/detections/detection.rs
@@ -209,7 +209,7 @@ impl Detection {
                 .to_string(),
             rule.yaml["title"].as_str().unwrap_or("").to_string(),
             rule.yaml["details"].as_str().unwrap_or("").to_string(),
-            tag_info.join("/"),
+            tag_info.join(" / "),
         );
     }
 
@@ -231,7 +231,7 @@ impl Detection {
             "-".to_owned(),
             rule.yaml["title"].as_str().unwrap_or("").to_owned(),
             output.to_owned(),
-            tag_info.join("/"),
+            tag_info.join(" / "),
         )
     }
 
