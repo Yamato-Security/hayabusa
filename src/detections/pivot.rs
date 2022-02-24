c04968bd55766d470d6992441de588067662636a
kazuminn <warugaki.k.k@gmail.com>
2022-02-24 10:41:14 +0900

    pass test in pivot.rs

--- src/detections/pivot.rs
+++ src/detections/pivot.rs
@@ -58,6 +58,27 @@ impl PivotKeyword {
     }
 
     pub fn insert_pivot_keyword(&mut self, event_record: &Value) {
+        let mut is_exist_event_key = false;
+        let mut tmp_event_record: &Value = event_record.into();
+        for s in ["Event", "System", "Level"] {
+            if let Some(record) = tmp_event_record.get(s) {
+                is_exist_event_key = true;
+                tmp_event_record = record;
+            }
+        }
+        if is_exist_event_key {
+            let hash_value = get_serde_number_to_string(tmp_event_record);
+
+            if hash_value.is_some() {
+                if hash_value.as_ref().unwrap() == "infomational"
+                    || hash_value.as_ref().unwrap() == "undefined"
+                    || hash_value.as_ref().unwrap() == "-"
+                {
+                    return ();
+                }
+            }
+        }
+
         for (key, fields) in self.fields.iter() {
             for field in fields {
                 if let Some(array_str) = configs::EVENTKEY_ALIAS.get_event_key(&String::from(field))
@@ -101,10 +122,17 @@ mod tests {
 
     #[test]
     fn insert_pivot_keyword_local_ip4() {
-        load_pivot_keywords("config/pivot_keywords.txt");
+        load_pivot_keywords("test_files/config/pivot_keywords.txt");
         let record_json_str = r#"
         {
-            "Event": {"EventData": {"IpAddress": "127.0.0.1"}}
+            "Event": { 
+                "System": {
+                    "Level": "high"
+                },
+                "EventData": {
+                    "IpAddress": "127.0.0.1"
+                }
+            }
         }"#;
         PIVOT_KEYWORD
             .write()
@@ -122,10 +150,17 @@ mod tests {
 
     #[test]
     fn insert_pivot_keyword_ip4() {
-        load_pivot_keywords("config/pivot_keywords.txt");
+        load_pivot_keywords("test_files/config/pivot_keywords.txt");
         let record_json_str = r#"
         {
-            "Event": {"EventData": {"IpAddress": "10.0.0.1"}}
+            "Event": { 
+                "System": {
+                    "Level": "high"
+                },
+                "EventData": {
+                    "IpAddress": "10.0.0.1"
+                }
+            }
         }"#;
         PIVOT_KEYWORD
             .write()
@@ -143,10 +178,17 @@ mod tests {
 
     #[test]
     fn insert_pivot_keyword_ip_empty() {
-        load_pivot_keywords("config/pivot_keywords.txt");
+        load_pivot_keywords("test_files/config/pivot_keywords.txt");
         let record_json_str = r#"
         {
-            "Event": {"EventData": {"IpAddress": "-"}}
+            "Event": { 
+                "System": {
+                    "Level": "high"
+                },
+                "EventData": {
+                    "IpAddress": "-"
+                }
+            }
         }"#;
         PIVOT_KEYWORD
             .write()
@@ -164,10 +206,17 @@ mod tests {
 
     #[test]
     fn insert_pivot_keyword_local_ip6() {
-        load_pivot_keywords("config/pivot_keywords.txt");
+        load_pivot_keywords("test_files/config/pivot_keywords.txt");
         let record_json_str = r#"
         {
-            "Event": {"EventData": {"IpAddress": "::1"}}
+            "Event": { 
+                "System": {
+                    "Level": "high"
+                },
+                "EventData": {
+                    "IpAddress": "::1"
+                }
+            }
         }"#;
         PIVOT_KEYWORD
             .write()
@@ -181,5 +230,89 @@ mod tests {
             .get_mut("Ip Addresses")
             .unwrap()
             .contains("::1"));
+    }
+
+    #[test]
+    fn insert_pivot_keyword_level_infomational() {
+        load_pivot_keywords("test_files/config/pivot_keywords.txt");
+        let record_json_str = r#"
+        {
+            "Event": { 
+                "System": {
+                    "Level": "infomational"
+                },
+                "EventData": {
+                    "IpAddress": "10.0.0.2"
+                }
+            }
+        }"#;
+        PIVOT_KEYWORD
+            .write()
+            .unwrap()
+            .insert_pivot_keyword(&serde_json::from_str(record_json_str).unwrap());
+
+        assert!(!PIVOT_KEYWORD
+            .write()
+            .unwrap()
+            .keywords
+            .get_mut("Ip Addresses")
+            .unwrap()
+            .contains("10.0.0.2"));
+    }
+
+    #[test]
+    fn insert_pivot_keyword_level_low() {
+        load_pivot_keywords("test_files/config/pivot_keywords.txt");
+        let record_json_str = r#"
+        {
+            "Event": { 
+                "System": {
+                    "Level": "low"
+                },
+                "EventData": {
+                    "IpAddress": "10.0.0.1"
+                }
+            }
+        }"#;
+        PIVOT_KEYWORD
+            .write()
+            .unwrap()
+            .insert_pivot_keyword(&serde_json::from_str(record_json_str).unwrap());
+
+        assert!(PIVOT_KEYWORD
+            .write()
+            .unwrap()
+            .keywords
+            .get_mut("Ip Addresses")
+            .unwrap()
+            .contains("10.0.0.1"));
+    }
+
+    #[test]
+    fn insert_pivot_keyword_level_none() {
+        load_pivot_keywords("test_files/config/pivot_keywords.txt");
+        let record_json_str = r#"
+        {
+            "Event": { 
+                "System": {
+                    "Level": "-"
+                },
+                "EventData": {
+                    "IpAddress": "10.0.0.3"
+                }
+            }
+        }"#;
+        PIVOT_KEYWORD
+            .write()
+            .unwrap()
+            .insert_pivot_keyword(&serde_json::from_str(record_json_str).unwrap());
+
+        assert!(!PIVOT_KEYWORD
+            .write()
+            .unwrap()
+            .keywords
+            .get_mut("Ip Addresses")
+            .unwrap()
+            .contains("10.0.0.3"));
     }
 }
