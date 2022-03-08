c33399b6815d0f3059b0be97ba8cd125e7efa82e
DustInDark <nextsasasa@gmail.com>
2022-03-08 20:43:42 +0900

    added checking EventData when key do not match in alias #290
    - added checking key in Event.EventData, if key is not exist in eventkey_alias.txt.

--- src/detections/utils.rs
+++ src/detections/utils.rs
@@ -166,8 +166,8 @@ pub fn get_event_value<'a>(key: &str, event_value: &'a Value) -> Option<&'a Valu
     }
 
     let event_key = configs::EVENTKEY_ALIAS.get_event_key(key);
+    let mut ret: &Value = event_value;
     if let Some(event_key) = event_key {
-        let mut ret: &Value = event_value;
         // get_event_keyが取得できてget_event_key_splitが取得できないことはない
         let splits = configs::EVENTKEY_ALIAS.get_event_key_split(key);
         let mut start_idx = 0;
@@ -184,8 +184,12 @@ pub fn get_event_value<'a>(key: &str, event_value: &'a Value) -> Option<&'a Valu
 
         Option::Some(ret)
     } else {
-        let mut ret: &Value = event_value;
-        let event_key = key;
+        let event_key;
+        if !key.contains('.') {
+            event_key = "Event.EventData.".to_string() + key;
+        } else {
+                event_key = key.to_string();
+        }
         for key in event_key.split('.') {
             if !ret.is_object() {
                 return Option::None;
