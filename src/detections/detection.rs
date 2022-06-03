86f3afab40ec57324fa546fed7383943d25a65de
DustInDark <nextsasasa@gmail.com>
2022-06-03 09:13:11 +0900

    cargo fmt

--- src/detections/detection.rs
+++ src/detections/detection.rs
@@ -369,7 +369,7 @@ impl Detection {
         println!("Rule parsing errors: {}", parseerror_count);
         println!();
         let mut sorted_rc: Vec<(&String, &u128)> = rc.iter().collect();
-        sorted_rc.sort_by(|a, b|a.0.cmp(b.0));
+        sorted_rc.sort_by(|a, b| a.0.cmp(b.0));
         let mut enable_total = 0;
         sorted_rc.into_iter().for_each(|(key, value)| {
             println!("{} rules: {}", key, value);
