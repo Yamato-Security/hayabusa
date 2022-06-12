7af4781134569f8e7f42f31fe29e909122f2d3fd
DustInDark <nextsasasa@gmail.com>
2022-06-12 22:50:59 +0900

    fixed colored contributors option bug

--- src/main.rs
+++ src/main.rs
@@ -424,7 +424,9 @@ impl App {
 
     fn print_contributors(&self) {
         match fs::read_to_string("./contributors.txt") {
-            Ok(contents) => println!("{}", contents),
+            Ok(contents) => {
+                write_color_buffer(BufferWriter::stdout(ColorChoice::Always), None, &contents).ok();
+            }
             Err(err) => {
                 AlertMessage::alert(&format!("{}", err)).ok();
             }
