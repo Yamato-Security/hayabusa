5d058d3b89a89153efd368213be3e99fa94bdf33
DustInDark <nextsasasa@gmail.com>
2022-06-26 00:50:05 +0900

    fixed test

--- src/afterfact.rs
+++ src/afterfact.rs
@@ -827,7 +827,7 @@ mod tests {
         let test_timestamp = Utc
             .datetime_from_str("1996-02-27T01:05:01Z", "%Y-%m-%dT%H:%M:%SZ")
             .unwrap();
-        let expect_header = "Timestamp|Computer|Channel|EventID|Level|RecordID|RuleTitle|Details\n";
+        let expect_header = "Timestamp|Computer|Channel|EventID|Level|RecordID|RuleTitle|Details";
         let expect_tz = test_timestamp.with_timezone(&Local);
 
         let expect_no_header = expect_tz
@@ -851,7 +851,7 @@ mod tests {
             + "|"
             + test_recinfo
             + "\n";
-        assert_eq!(_get_serialized_disp_output(None,), expect_header);
+        assert_eq!(_get_serialized_disp_output(None), expect_header);
         assert_eq!(
             _get_serialized_disp_output(Some(DisplayFormat {
                 timestamp: &format_time(&test_timestamp, false),
