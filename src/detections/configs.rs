6e555f0ec2d493d36761d0807eb99aec81f0a426
DustInDark <nextsasasa@gmail.com>
2022-06-28 23:52:10 +0900

    changed D option and added deep-scan option #608

--- src/detections/configs.rs
+++ src/detections/configs.rs
@@ -115,8 +115,12 @@ pub struct Config {
     pub visualize_timeline: bool,
 
     /// Enable rules marked as deprecated
-    #[clap(short = 'D', long = "enable-deprecated-rules")]
+    #[clap(long = "enable-deprecated-rules")]
     pub enable_deprecated_rules: bool,
+
+    /// Scan to all event ID
+    #[clap(short = "D", long="deep-scan")]
+    pub deep_scan: bool,
 
     /// Enable rules marked as noisy
     #[clap(short = 'n', long = "enable-noisy-rules")]
