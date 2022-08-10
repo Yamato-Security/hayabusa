09240212e87627fc4e32355c2d6ca9575ddea330
DastInDark <2350416+hitenkoku@users.noreply.github.com>
2022-08-10 20:44:26 +0900

    add new line help

--- src/detections/configs.rs
+++ src/detections/configs.rs
@@ -57,8 +57,9 @@ impl Default for ConfigReader<'_> {
     name = "Hayabusa",
     usage = "hayabusa.exe <INPUT> [OTHER-ACTIONS] [OPTIONS]",
     author = "Yamato Security (https://github.com/Yamato-Security/hayabusa) @SecurityYamato)",
+    help_template = "\n{name}\n{author}\n\n{usage-heading}\n    {usage}\n\n{all-args}\n",
     version,
-    arg_required_else_help = true,
+    arg_required_else_help (true),
     term_width = 400
 )]
 pub struct Config {
