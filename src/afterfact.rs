11adcee8cb40ab846d0053e83ff47f6098e00d56
DustInDark <2350416+hitenkoku@users.noreply.github.com>
2022-12-28 15:09:43 +0900

    fix(jsontmline): remove newline when -J opt enable #852

--- src/afterfact.rs
+++ src/afterfact.rs
@@ -1212,7 +1212,11 @@ fn output_json_str(ext_field: &[(CompactString, Profile)], jsonl_output_flag: bo
                         }
                     }
                     output_stock.push("    }".to_string());
-                    target.push(output_stock.join("\n"));
+                    if jsonl_output_flag {
+                        target.push(output_stock.join(""));
+                    } else {
+                        target.push(output_stock.join("\n"));
+                    }
                 }
                 Profile::MitreTags(_) | Profile::MitreTactics(_) | Profile::OtherTags(_) => {
                     let key = _convert_valid_json_str(&[key.as_str()], false);
