d0f3813a4e45bb5095ffc6ef454a1248643a77b2
Yamato Security <71482215+YamatoSecurity@users.noreply.github.com>
2023-03-03 07:49:37 +0900

    fix error msg

--- src/main.rs
+++ src/main.rs
@@ -940,7 +940,7 @@ impl App {
 
         if rule_files.is_empty() {
             AlertMessage::alert(
-                "No rules were loaded. Please download the latest rules with the --update-rules option.\r\n",
+                "No rules were loaded. Please download the latest rules with the update-rules command.\r\n",
             )
             .ok();
             return;
