5db01b7d4820f8d4499a86c76d820317aac87756
DastInDark <2350416+hitenkoku@users.noreply.github.com>
2023-05-15 12:40:01 +0900

    refactor(afterfact): fixed run clippy unused-doc-comments

--- src/afterfact.rs
+++ src/afterfact.rs
@@ -1521,7 +1521,7 @@ fn output_detected_rule_authors(rule_author_counter: HashMap<CompactString, i128
         let mut tmp = Vec::new();
         for y in 0..div {
             if y * 4 + x < sorted_authors.len() {
-                /// Limit length to 27 to prevent the table from wrapping
+                // Limit length to 27 to prevent the table from wrapping
                 let filter_author = if sorted_authors[y * 4 + x].0.len() <= 27 {
                     sorted_authors[y * 4 + x].0.to_string()
                 } else {
