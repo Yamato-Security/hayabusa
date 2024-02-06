52000b464cbd717c3fc25b1e38743702191c185f
DustInDark <2350416+hitenkoku@users.noreply.github.com>
2024-02-06 09:27:45 +0900

    UI(afterfact): changed rule title color in csv-timeline command(adapted commend) #1271

--- src/afterfact.rs
+++ src/afterfact.rs
@@ -963,7 +963,7 @@ fn _get_serialized_disp_output(
                 col_pos,
             );
             let output_color_and_contents = match d.1 {
-                Profile::Timestamp(_) | Profile::Level(_) => {
+                Profile::Timestamp(_) | Profile::Level(_) | Profile::RuleTitle(_) => {
                     vec![vec![(
                         display_contents,
                         get_writable_color(level_color, no_color),
