7fe43a0a713c86f68b9e8414d23d18f5be2d1ca0
fukusuket <41001169+fukusuket@users.noreply.github.com>
2024-09-01 20:06:23 +0900

    feat: add hayabusa rule count and logging.

--- doc/SupportedSigmaFieldModifiers.md
+++ doc/SupportedSigmaFieldModifiers.md
@@ -1,15 +1,31 @@
-|   Count | Field Modifier        | Hayabusa Support   |
-|--------:|:----------------------|:-------------------|
-|      13 | all                   | Yes                |
-|       7 | base64offsetǀcontains | Yes                |
-|      34 | cidr                  | Yes                |
-|    2738 | contains              | Yes                |
-|     970 | containsǀall          | Yes                |
-|       4 | containsǀallǀwindash  | Yes                |
-|       1 | containsǀexpand       | No                 |
-|      76 | containsǀwindash      | Yes                |
-|    2893 | endswith              | Yes                |
-|       1 | endswithǀwindash      | Yes                |
-|       8 | expand                | No                 |
-|     165 | re                    | Yes                |
-|     436 | startswith            | Yes                |
+|   Sigma Count |   Hayabusa Count | Field Modifier        | Hayabusa Support   |
+|--------------:|-----------------:|:----------------------|:-------------------|
+|            13 |               13 | all                   | Yes                |
+|             7 |               11 | base64offsetǀcontains | Yes                |
+|            34 |               62 | cidr                  | Yes                |
+|          2738 |             4512 | contains              | Yes                |
+|           970 |             1653 | containsǀall          | Yes                |
+|             4 |                8 | containsǀallǀwindash  | Yes                |
+|             1 |                0 | containsǀexpand       | No                 |
+|            76 |              150 | containsǀwindash      | Yes                |
+|          2893 |             4683 | endswith              | Yes                |
+|             1 |                2 | endswithǀwindash      | Yes                |
+|             8 |                0 | expand                | No                 |
+|           165 |              237 | re                    | Yes                |
+|           436 |              542 | startswith            | Yes                |
+|             0 |                0 | exists                | Yes                |
+|             0 |                0 | cased                 | Yes                |
+|             0 |                0 | reǀi                  | Yes                |
+|             0 |                0 | reǀm                  | Yes                |
+|             0 |                0 | reǀs                  | Yes                |
+|             0 |                0 | base64ǀutf16le        | No                 |
+|             0 |                0 | base64ǀutf16be        | No                 |
+|             0 |                0 | base64ǀwide           | No                 |
+|             0 |                0 | lt                    | No                 |
+|             0 |                0 | lte                   | No                 |
+|             0 |                0 | gt                    | No                 |
+|             0 |                0 | gte                   | No                 |
+|             0 |                0 | fieldref              | No                 |
+
+Updated: 2024/09/01
+Author: Fukusuke Takahashi
--- scripts/supported_modifiers_check/supported-modifier.py
+++ scripts/supported_modifiers_check/supported-modifier.py
@@ -2,11 +2,16 @@ import argparse
 import os
 import glob
 import re
+import datetime
+import logging
+import time
 from pathlib import Path
 
 import ruamel.yaml
 import pandas as pd
 from collections import Counter
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 def extract_keys_recursive(d) -> list[str]:
@@ -24,13 +29,22 @@ def extract_keys_recursive(d) -> list[str]:
     return keys
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Process Sigma YAML files and generate a markdown report.')
-    parser.add_argument('sigma_path', type=str, help='Directory containing Sigma YAML files')
-    parser.add_argument('out_path', type=str, help='Path to save the generated markdown file')
-    args = parser.parse_args()
+def add_missing_modifiers(counter: Counter) -> Counter:
+    check_strings = [
+        'all', 'startswith', 'endswith', 'contains', 'exists', 'cased', 'windash', 're', 're|i', 're|m', 're|s',
+        'base64', 'base64offset', 'base64|utf16le', 'base64|utf16be', 'base64|utf16', 'base64|wide',
+        'lt', 'lte', 'gt', 'gte', 'cidr', 'expand', 'fieldref'
+    ]
 
-    yml_files = glob.glob(os.path.join(args.sigma_path, '**', '*.yml'), recursive=True)
+    for key in check_strings:
+        if not any(key in s for s in counter.keys()):
+            counter[key] = 0
+    return counter
+
+
+def get_yml_detection_counts(dir_path: str) -> Counter:
+    logging.info(f'Starting to process YAML files in directory: {dir_path}')
+    yml_files = glob.glob(os.path.join(dir_path, '**', '*.yml'), recursive=True)
     yml_detection_keys = []
     for file in yml_files:
         with open(file, 'r') as f:
@@ -38,15 +52,39 @@ if __name__ == '__main__':
             for content in contents:
                 if content.get('logsource', {}).get('product') == 'windows':
                     yml_detection_keys.extend(extract_keys_recursive(content.get('detection', {})))
+    logging.info('Finished processing YAML files')
+    return add_missing_modifiers(Counter(sorted(yml_detection_keys)))
 
-    key_counter = Counter(sorted(yml_detection_keys))
-    header = ["Count", "Field Modifier", "Hayabusa Support"]
-    hayabusa_supported = {"all", "base64offset", "contains", "cidr", "windash", "endswith", "startswith", "re"}
+
+if __name__ == '__main__':
+    start_time = time.time()
+    logging.info(f'Starting script execution: {os.path.basename(__file__)}')
+    parser = argparse.ArgumentParser(description='Process Sigma YAML files and generate a markdown report.')
+    parser.add_argument('sigma_path', type=str, help='Directory containing Sigma YAML files')
+    parser.add_argument('hayabusa_path', type=str, help='Directory containing Hayabusa YAML files')
+    parser.add_argument('out_path', type=str, help='Path to save the generated markdown file')
+    args = parser.parse_args()
+
+    sigma_key_counter = get_yml_detection_counts(args.sigma_path)
+    hayabusa_key_counter = get_yml_detection_counts(args.hayabusa_path)
+    header = ["Sigma Count", "Hayabusa Count", "Field Modifier", "Hayabusa Support"]
+    hayabusa_supported = {"all", "base64offset", "contains", "cidr", "windash", "endswith", "startswith", "re", "exists", "cased", "re", "re|i", "re|m", "re|s"}
+
     result = []
-    for k, v in key_counter.items():
+    for k, v in sigma_key_counter.items():
         modifiers = [x for x in str(k).split('|') if x]
         supported_modifier = all(map(lambda x: True if x in hayabusa_supported else False, modifiers))
         supported_modifier = "Yes" if supported_modifier else "No"
-        result.append([v, k.strip('|').replace('|', 'ǀ'), supported_modifier])
+        supported_modifier = "Yes" if k in hayabusa_supported else supported_modifier
+        hayabusa_count = hayabusa_key_counter.get(k, 0)
+        result.append([v, hayabusa_count, k.strip('|').replace('|', 'ǀ'), supported_modifier])
+
     markdown_str = pd.DataFrame(result, columns=header).to_markdown(index=False)
+    formatted_datetime = datetime.datetime.now().strftime('%Y/%m/%d')
+    markdown_str = f"{markdown_str}\n\nUpdated: {formatted_datetime}\nAuthor: Fukusuke Takahashi"
     Path(args.out_path).write_text(markdown_str)
+    end_time = time.time()
+    execution_time = end_time - start_time
+
+    logging.info(f'Markdown report generated and saved to {args.out_path}')
+    logging.info(f'Script execution completed in {execution_time:.2f} seconds')
