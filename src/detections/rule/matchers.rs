fbdadd60e2b62af0d75a8d19c82006a683e8ad8d
fukusuket <41001169+fukusuket@users.noreply.github.com>
2024-10-11 14:22:13 +0900

    chg: Create fieldref|endswith alias for endswithfield

--- src/detections/rule/matchers.rs
+++ src/detections/rule/matchers.rs
@@ -362,15 +362,20 @@ impl LeafMatcher for DefaultMatcher {
         if keys_all[0].is_empty() && keys_all.len() == 2 && keys_all[1] == "all" {
             keys_all[1] = change_map["all"];
         }
-        if keys_all.len() >= 3 && keys_all[1] == "re" {
-            if keys_all[2] == "i" {
-                keys_all[2] = change_map["i"];
-            } else if keys_all[2] == "m" {
-                keys_all[2] = change_map["m"];
-            } else if keys_all[2] == "s" {
-                keys_all[2] = change_map["s"];
+        if keys_all.len() >= 3 {
+            if keys_all[1] == "re" {
+                if keys_all[2] == "i" {
+                    keys_all[2] = change_map["i"];
+                } else if keys_all[2] == "m" {
+                    keys_all[2] = change_map["m"];
+                } else if keys_all[2] == "s" {
+                    keys_all[2] = change_map["s"];
+                }
+                keys_all.remove(1);
+            } else if keys_all[1] == "fieldref" && keys_all[2] == "endswith" {
+                keys_all[1] = "fieldrefendswith";
+                keys_all.remove(2);
             }
-            keys_all.remove(1);
         }
 
         let keys_without_head = &keys_all[1..];
@@ -741,6 +746,7 @@ impl PipeElement {
             "equalsfield" => Option::Some(PipeElement::EqualsField(pattern.to_string())),
             "endswithfield" => Option::Some(PipeElement::Endswithfield(pattern.to_string())),
             "fieldref" => Option::Some(PipeElement::FieldRef(pattern.to_string())),
+            "fieldrefendswith" => Option::Some(PipeElement::Endswithfield(pattern.to_string())), // endswithfieldを廃止したらfieldrefに置き換え
             "base64offset" => Option::Some(PipeElement::Base64offset),
             "windash" => Option::Some(PipeElement::Windash),
             "cidr" => Option::Some(PipeElement::Cidr(IpCidr::from_str(pattern))),
