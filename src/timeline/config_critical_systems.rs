3684a0850319ae69fd01d2c481b6dd9776215f62
fukusuket <41001169+fukusuket@users.noreply.github.com>
2025-02-15 21:51:41 +0900

    chg: found msgs

--- src/timeline/config_critical_systems.rs
+++ src/timeline/config_critical_systems.rs
@@ -98,7 +98,7 @@ impl ConfigCriticalSystems {
                     write_color_buffer(
                         &BufferWriter::stdout(ColorChoice::Always),
                         get_writable_color(Some(Color::Rgb(0, 255, 0)), no_color),
-                        &format!("{:?} found ({:?}):", computer_type.to_str(), names.len()),
+                        &format!("{:?} found ({:?}):", computer_type.to_str().replace("\"",""), names.len()),
                         true,
                     )
                     .ok();
