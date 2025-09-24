f9493b7c8339ea5bee5d07e454558cebf75a2495
fukusuket <41001169+fukusuket@users.noreply.github.com>
2025-09-24 20:47:52 +0900

    fix: revert trim process

--- src/afterfact.rs
+++ src/afterfact.rs
@@ -1866,7 +1866,7 @@ pub fn output_json_str(
         .collect();
     for (key, profile) in target_ext_field.iter() {
         let val = profile.to_value();
-        if val.trim().is_empty() {
+        if val.is_empty() {
             continue;
         }
         let vec_data = _get_json_vec(profile, &val.to_string());
